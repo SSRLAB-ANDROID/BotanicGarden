770598f559ff157aab9b1849c26a94d30f832136
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-07-20 10:13:43 +0300

    Start and info screens has been initialized

---
+++ app/src/main/java/com/ssrlab/audioguide/botanic/ui/FragmentInfo.kt
@@ -0,0 +1,24 @@
+package com.ssrlab.audioguide.botanic.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ssrlab.audioguide.botanic.databinding.FragmentInfoBinding
+
+class FragmentInfo: Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentInfoBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
