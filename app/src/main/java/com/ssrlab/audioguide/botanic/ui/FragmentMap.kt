e1d5afab0299eb6f5eca8aef913e9666eae46cda
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-08-14 23:30:24 +0300

    Map location initialization

--- app/src/main/java/com/ssrlab/audioguide/botanic/ui/FragmentMap.kt
+++ app/src/main/java/com/ssrlab/audioguide/botanic/ui/FragmentMap.kt
@@ -6,15 +6,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.locationcomponent.location
 import com.ssrlab.audioguide.botanic.MainActivity
 import com.ssrlab.audioguide.botanic.R
 import com.ssrlab.audioguide.botanic.databinding.FragmentMapBinding
+import com.ssrlab.audioguide.botanic.vm.ExhibitViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -26,6 +36,9 @@ class FragmentMap: Fragment() {
     private lateinit var mainActivity: MainActivity
     private var permissionConstant = false
     private var mapView: MapView? = null
+
+    private val viewModel: ExhibitViewModel by activityViewModels()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val scope = CoroutineScope(Dispatchers.IO)
 
@@ -47,19 +60,38 @@ class FragmentMap: Fragment() {
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
 
         checkPermission()
-        setUpActions()
+        setLocationAction()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(mainActivity)
+
+        binding.mapBack.setOnClickListener {
+            mainActivity.onBackPressedDispatcher.onBackPressed()
+        }
 
         return binding.root
     }
 
-    private fun setUpActions() {
+    private fun setLocationAction() {
         scope.launch {
             if (permissionConstant) {
+
+                mainActivity.runOnUiThread{
+                    setUpMapBox()
+                }
+
                 binding.mapPosition.apply {
                     binding.mapPositionIc.setImageResource(R.drawable.ic_location_enabled)
                     background = ContextCompat.getDrawable(mainActivity, R.drawable.background_green_button)
                     setOnClickListener {
-                        Toast.makeText(mainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
+
+                        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                                if (location != null) {
+                                    val cameraSettings = cameraOptions {
+                                        center(Point.fromLngLat(location.longitude, location.latitude))
+                                    }
+                                    mapView?.camera?.flyTo(cameraSettings)
+                                }
+                            }
                     }
                 }
             } else {
@@ -72,8 +104,34 @@ class FragmentMap: Fragment() {
 
             delay(2000)
             checkPermission()
-            setUpActions()
+            setLocationAction()
         }
+    }
+
+    private fun setUpMapBox() {
+        mapView?.location?.apply {
+            enabled = true
+            pulsingEnabled = true
+            pulsingColor = ContextCompat.getColor(mainActivity, R.color.map_red)
+        }
+
+        mapView?.location?.locationPuck = LocationPuck2D(
+            topImage = AppCompatResources.getDrawable(mainActivity, R.drawable.ic_location_marker),
+            bearingImage = AppCompatResources.getDrawable(mainActivity, R.drawable.ic_location_marker),
+            shadowImage = AppCompatResources.getDrawable(mainActivity, R.drawable.ic_location_marker),
+            scaleExpression = interpolate {
+                linear()
+                zoom()
+                stop {
+                    literal(0.0)
+                    literal(0.6)
+                }
+                stop {
+                    literal(20.0)
+                    literal(1.0)
+                }
+            }.toJson()
+        )
     }
 
     private fun requestLocationPermission() {
